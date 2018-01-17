d4ae16eda6025484ac587455ab1db1492f6f0c2d
Kathy Hudson <khitx@icloud.com>
2018-01-17 19:40:09 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -91,8 +91,6 @@ def adding_matz
 
 end
 
-#puts adding_matz()
-
 def changing_alan
 	programmer_hash =
  		{
@@ -112,6 +110,8 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
 		 programmer_hash[:alan_kay][:known_for] = alans_new_info
+
+		 programmer_hash
 
 end
 
@@ -133,4 +133,6 @@ def adding_to_dennis
      }
 
 	programmer_hash[:dennis_ritchie][:languages] << 'Assembly'
+
+	programmer_hash
 end
